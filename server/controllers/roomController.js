f5ebd640d4272c5deada8b4d21ff03a7d8317f43
KumarAkash10 <akashkumarabhi10@gmail.com>
2025-06-17 02:04:17 +0530

    Add Hotel & Room model and controllers

---
+++ server/controllers/roomController.js
@@ -0,0 +1,36 @@
+//API to create a new room for a hotel
+export const createRoom = async(req, res)=>{
+    try {
+        const {roomType, pricePerNight, amenities} = req.body;
+        
+    } catch (error) {
+        
+    }
+}
+
+//API to get all rooms
+export const getRooms = async(req, res)=>{
+    try {
+        
+    } catch (error) {
+        
+    }
+}
+
+//Api to get all rooms for a specific hotel
+export const getOwnerRooms = async(req, res)=>{
+    try {
+        
+    } catch (error) {
+        
+    }
+}
+
+//API to toggle availability of a room
+export const toggleRoomAvailability = async(req, res)=>{
+    try {
+        
+    } catch (error) {
+        
+    }
+}
